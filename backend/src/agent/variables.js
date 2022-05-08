e90770ed6439f4a387528ff12dc8efad1d061f13
Luis Villamil <villamil_50@hotmail.com>
2022-05-08 18:05:50 -0500

    agent

--- backend/src/agent/variables.js
+++ backend/src/agent/variables.js
@@ -6,7 +6,12 @@ export let variables;
 export const setVariables = () => {
   const configPath = process.argv[2];
   console.log("Config Path", configPath);
-  const doc = fs.readFileSync(configPath, "utf8");
+  let doc;
+  try {
+    doc = fs.readFileSync(configPath, "utf8");
+  } catch (error) {
+    console.log(error);
+  }
   variables = toJson(doc);
   console.log(variables);
 };
